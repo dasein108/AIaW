def14bba367c3d119d01c4b1e6f2ae97e71d5163
dasein <acidpictures@gmail.com>
2025-05-11 17:37:45 +0530

    chore: update ESLint configuration to include comma-dangle rule
    - Added 'comma-dangle' rule with a warning level to the ESLint configuration for improved code style consistency.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -105,6 +105,7 @@ module.exports = {
     '@typescript-eslint/no-explicit-any': 'off',
 
     // allow debugger during development only
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'comma-dangle': 'warn',
   }
 }
